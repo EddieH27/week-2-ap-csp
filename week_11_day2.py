e541ec4b758a30904cfdfdbdc97dc26163c49a14
EddieH27 <141260227+EddieH27@users.noreply.github.com>
2025-10-31 17:40:17 +0000

    bell ringer

--- week_11_day2.py
+++ week_11_day2.py
@@ -9,20 +9,21 @@ print("You’ll review variables, strings, numbers, and print formatting.\n")
 
 # Step 1: Create Variables
 # TODO: Replace the values below with your own info
-first_name = "Marvin"
-age = 30
-favorite_color = "Blue"
-favorite_number = 7
+first_name = "Eduardo"
+age = 17
+favorite_color = "purple"
+favorite_number = 69
 
 #  Step 2: Practice String Operations
 # 1. Print your name in uppercase
+print(first_name.upper())
 
 
 # 2. Print how many letters are in your name
-
+print(len(first_name))
 
 # 3. Combine your name and favorite color into one message
-
+print("My favorite color is " + favorite_color + " and " + "my favorite number is " + str(favorite_number))
 
 #  Step 3: Math Practice
 # Use arithmetic operators with your favorite number
